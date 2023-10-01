f782034399fb26fd4051e3f41e5d63b922bb6fa9
abdonaser <abdonaser4223@gmail.com>
2023-10-01 05:00:03 +0300

    Configure React app for deployment to GitHub Pages

--- src/Components/Notfound/NotFound.jsx
+++ src/Components/Notfound/NotFound.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
 
 export default class NotFound extends Component {
     render() {
         return <>
-            <div className='text-bg-dark  d-flex justify-content-center align-items-center '>
+            <div className='text-bg-dark not-found   d-flex justify-content-center align-items-center '>
                 <h1 className=' text-center   p-2 '> Not  Found</h1>
             </div>
 
